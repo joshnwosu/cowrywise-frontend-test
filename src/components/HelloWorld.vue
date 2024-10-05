f93d1b3127ed2516615b125c9a8d8166e41c0e85
joshua <joshuanwosu078@gmail.com>
2024-10-05 03:25:38 +0100

    setup

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="count++">count! is {{ count }}</button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+  </div>
+
+  <p>
+    Check out
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+      >create-vue</a
+    >, the official Vue + Vite starter
+  </p>
+  <p>
+    Learn more about IDE Support for Vue in the
+    <a
+      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
+      target="_blank"
+      >Vue Docs Scaling up Guide</a
+    >.
+  </p>
+  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+
+
+  <div class="main">
+    <div class="main-inner">
+      <h1>Title</h1>
+      <p>This is the body</p>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.read-the-docs {
+  color: #888;
+}
+
+
+.main {
+  background-color: red;
+  padding: 20px;
+
+  .main-inner {
+    background-color: purple;
+  }
+}
+</style>
