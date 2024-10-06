47313affdb2c2b90961062dc7ffa23394d335191
joshua <joshuanwosu078@gmail.com>
2024-10-07 00:38:42 +0100

    update: masonry layout style

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -3,7 +3,7 @@
 
 
         <svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg" class="search-icon">
+            xmlns="http://www.w3.org/2000/svg" class="search-icon" @click="onSearch">
             <path
                 d="m15.97 17.031c-1.479 1.238-3.384 1.985-5.461 1.985-4.697 0-8.509-3.812-8.509-8.508s3.812-8.508 8.509-8.508c4.695 0 8.508 3.812 8.508 8.508 0 2.078-.747 3.984-1.985 5.461l4.749 4.75c.146.146.219.338.219.531 0 .587-.537.75-.75.75-.192 0-.384-.073-.531-.22zm-5.461-13.53c-3.868 0-7.007 3.14-7.007 7.007s3.139 7.007 7.007 7.007c3.866 0 7.007-3.14 7.007-7.007s-3.141-7.007-7.007-7.007z"
                 fill-rule="nonzero" />
@@ -14,10 +14,13 @@
             @keyup.enter="onSearch" />
     </div>
 
+    <!-- Conditionally show "Press enter to search" message -->
+    <p v-if="showPressEnterMessage" class="press-enter-message">- Press Enter to search</p>
+
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import { computed, defineComponent, ref } from 'vue';
 
 export default defineComponent({
     name: 'SearchBar',
@@ -25,12 +28,17 @@ export default defineComponent({
     setup(_, { emit }) {
         const searchQuery = ref('');
 
+        // Computed property to check if there's input and show the message
+        const showPressEnterMessage = computed(() => searchQuery.value.length > 0);
+
+
         const onSearch = () => {
             emit('search', searchQuery.value);
         };
 
         return {
             searchQuery,
+            showPressEnterMessage,
             onSearch,
         };
     },
@@ -74,5 +82,19 @@ export default defineComponent({
     width: 24px;
     height: 24px;
     z-index: 2;
+    cursor: pointer;
+}
+
+.press-enter-message {
+
+    font-size: 16px;
+    color: #72788A;
+    transition: opacity 0.3s;
+    font-style: italic;
+    font-weight: 600;
+    margin-top: 5px;
+    margin-left: 10px;
+
+    position: absolute;
 }
 </style>
--- src/components/shared/Container.vue
+++ src/components/shared/Container.vue
@@ -9,7 +9,7 @@
     position: relative;
     width: 1000px;
     max-width: 100%;
-    margin: 70px auto 0;
+    margin: 50px auto 0;
     padding: 30px;
     z-index: 2;
 }
